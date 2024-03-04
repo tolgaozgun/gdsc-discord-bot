09f4b5a33f41e0f93de160e37a395b92ba02676f
Tolga Özgün <tolgaozgunn@gmail.com>
2024-03-04 23:36:22 +0300

    Update chromium

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 import time
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
+
 
 
 dotenv.load_dotenv()
@@ -99,8 +102,11 @@ async def register(interaction:discord.Interaction, url: str):
     
     # Check for devsite-profiles-splash--text class in the page
     options = Options()
-    options.headless = True
-    wd = webdriver.Chrome(options=options)
+    options.add_argument("--headless")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    wd = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
     wd.get(url)
     time.sleep(8)  # Allow time for the page to load
     soup = BeautifulSoup(wd.page_source, 'lxml')
