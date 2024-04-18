5c195219fbb4c2251369af920eec90f634ade57a
Tolga Özgün <tolgaozgunn@gmail.com>
2024-04-18 22:33:11 +0300

    Update course content, add DS_Store to gitignore

--- utils/constants.py
+++ utils/constants.py
@@ -1,10 +1,12 @@
-
 BADGES = ["Introduction to Image Generation", 
         "Attention Mechanism", 
         "Encoder-Decoder Architecture",
         "Transformer Models and BERT Model",
         "Create Image Captioning Models",
-        "Introduction to Generative AI Studio",
-        "Generative AI Explorer - Vertex AI",
-        "Explore and Evaluate Models using Model Garden",
-        "Prompt Design using PaLM"] 
+        "Introduction to Vertex AI Studio",
+        "Vector Search and Embeddings",
+        "Inspect Rich Documents with Gemini Multimodality and Multimodal RAG",
+        "Responsible AI for Developers: Fairness & Bias",
+        "Responsible AI for Developers: Interpretability & Transparency",
+        "Machine Learning Operations (MLOps) for Generative AI"
+        ] 
